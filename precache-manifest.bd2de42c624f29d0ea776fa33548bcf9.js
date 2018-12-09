5e0a32714d3e06abfbf986246162373ee67ccbb8
Alexandra Dinsdale <sandra@spotify.com>
2018-12-09 11:32:28 +0100

    Updates

---
+++ precache-manifest.bd2de42c624f29d0ea776fa33548bcf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cbd92f46d890f059e4f",
+    "url": "/protool/static/js/runtime~main.2cbd92f4.js"
+  },
+  {
+    "revision": "6892c971cf75b48ec3bb",
+    "url": "/protool/static/js/main.6892c971.chunk.js"
+  },
+  {
+    "revision": "cafbe54b78ff807d092f",
+    "url": "/protool/static/js/1.cafbe54b.chunk.js"
+  },
+  {
+    "revision": "6892c971cf75b48ec3bb",
+    "url": "/protool/static/css/main.9acd51b9.chunk.css"
+  },
+  {
+    "revision": "c137ce3e47eabbd5c628b5baf9e28323",
+    "url": "/protool/index.html"
+  }
+];
